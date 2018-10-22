7ab448ce5b4cd513a801bd8f43f16850612ca693
Mike Dilley <mike.dilley@gmail.com>
2018-10-22 22:49:47 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer unless project.backer == self
+    project.add_backer unless project.backers == self
   end
 
 end
